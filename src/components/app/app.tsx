80a34eab28d5de10b864da6a515541eb8332f211
tkornilova <kornilova.tat07@gmail.com>
2024-02-23 16:54:13 +0400

    feat: подключает маршрутизацию для страниц Main, Login, Favorites, Offer

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,9 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../const';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Offer from '../../pages/offer/offer';
 
 type AppProps = {
   placesCount: number;
@@ -6,7 +11,26 @@ type AppProps = {
 
 function App({placesCount}: AppProps): JSX.Element {
   return (
-    <Main placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main placesCount={placesCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<Offer />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/const.ts
+++ src/components/const.ts
@@ -1,3 +1,10 @@
 export const Settings = {
   placesCount: 312,
 } as const;
+
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+}
