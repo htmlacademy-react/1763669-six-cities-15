c9add1d466a0f437006ad4a086370ca4750d04e2
tkornilova <kornilova.tat07@gmail.com>
2024-03-10 21:20:08 +0400

    feat: добавляет обработчик введенных данных для формы

--- src/components/blocks/place-сard/place-сard.tsx
+++ src/components/blocks/place-сard/place-сard.tsx
@@ -17,6 +17,7 @@ function PlaceCard(props: PlaceCardProps): JSX.Element {
     // console.log('active card:', activeCardId);
     return activeCardId;
   };
+
   return (
     <article
       className={classNames([
--- src/components/consts.ts
+++ src/components/consts.ts
@@ -14,3 +14,31 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
+
+export const STAR_RATING = [
+  {
+    value: '5',
+    id: '5-stars',
+    title: 'perfect'
+  },
+  {
+    value: '4',
+    id: '4-stars',
+    title: 'good'
+  },
+  {
+    value: '3',
+    id: '3-stars',
+    title: 'not bad'
+  },
+  {
+    value: '2',
+    id: '2-stars',
+    title: 'badly'
+  },
+  {
+    value: '1',
+    id: '1-star',
+    title: 'terribly'
+  },
+];
