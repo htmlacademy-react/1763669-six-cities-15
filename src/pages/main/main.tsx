1053e3633f1a4e35195a90a86281338f010dc6fa
tkornilova <kornilova.tat07@gmail.com>
2024-04-03 17:13:08 +0400

    feat: добавляет состояние main--empty

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,5 +1,6 @@
 import { useAppSelector } from '../../store/useAppDispatch';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
 import PlaceCards from '../../components/blocks/place-cards/place-cards';
 import Map from '../../components/blocks/map/map';
@@ -54,7 +55,13 @@ function Main(): JSX.Element {
         </div>
       </header>
 
-      <main className="page__main page__main--index">
+      <main
+        className={classNames([
+          'page__main',
+          'page__main--index',
+          currentOffers.length === 0 ? 'page__main--index-empty' : '',
+        ])}
+      >
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -62,17 +69,27 @@ function Main(): JSX.Element {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{ currentOffers.length } places to stay in { currentCity }</b>
-              <Sorting />
-              <PlaceCards cards={ currentOffers } />
-            </section>
-            <div className="cities__right-section">
-              <Map city={ CITIES[cityIndex] } points={ currentOffers } activeOfferId={ activeOfferId }/>
-            </div>
-          </div>
+          {currentOffers.length === 0 ?
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+                </div>
+              </section>
+              <div className="cities__right-section"></div>
+            </div> :
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{ currentOffers.length } places to stay in { currentCity }</b>
+                <Sorting />
+                <PlaceCards cards={ currentOffers } />
+              </section>
+              <div className="cities__right-section">
+                <Map city={ CITIES[cityIndex] } points={ currentOffers } activeOfferId={ activeOfferId }/>
+              </div>
+            </div>}
         </div>
       </main>
     </div>
