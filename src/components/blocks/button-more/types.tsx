4a466f74bc66942af9d0c0558ce402b85f478838
tkornilova <kornilova.tat07@gmail.com>
2024-04-08 19:51:32 +0400

    feat: добавляет компонент ButtonMore

---
+++ src/components/blocks/button-more/types.tsx
@@ -0,0 +1,4 @@
+export type ButtonMoreProps = {
+  onClick: () => void;
+  isDisabled: boolean;
+}
--- src/components/blocks/reviews/reviews.tsx
+++ src/components/blocks/reviews/reviews.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react';
+import { useAppSelector } from '../../../store/useAppDispatch';
+
 import { ReviewsProps } from './types';
 import { ReviewProps } from '../review/types';
 import MemoizedReview from '../review/review';
+import ButtonMore from '../button-more/button-more';
 
 function Reviews({ items }: ReviewsProps): JSX.Element {
+  const [loadedCommentsCount, setLoadedCommentsCount] = useState(10);
+  const reviews = useAppSelector((state) => state.reviews);
+
+  const loadMoreComments = () => {
+    setLoadedCommentsCount(loadedCommentsCount + 10);
+  };
+
+  const remainingComments = reviews ? reviews.length - loadedCommentsCount : 0;
+  const isDisabled = remainingComments <= 0;
+
   return (
-    <ul className="reviews__list">
-      {items.map((card: ReviewProps) => (
-        <MemoizedReview { ...card } key={ card.id }/>
-      ))}
-    </ul>
+    <>
+      <ul className="reviews__list">
+        {
+          items.slice(0, loadedCommentsCount).map((card: ReviewProps) => (
+            <MemoizedReview { ...card } key={ card.id }/>
+          ))
+        }
+      </ul>
+      {
+        (reviews?.length ?? 0) > 10 &&
+          <ButtonMore onClick={ loadMoreComments } isDisabled={ isDisabled }/>
+      }
+    </>
   );
 }
 
